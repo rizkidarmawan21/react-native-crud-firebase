796c9a8f4e86f123e8114a9b11989f9cd7d4ff22
Rizki Darmawan <rizkidarmawan.0402102@gmail.com>
2021-12-24 13:28:56 +0700

    initial commit

---
+++ src/router/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import Home from '../pages/Home'
+const Stack = createNativeStackNavigator();
+
+const Router = () => {
+    return (
+        <Stack.Navigator>
+            <Stack.Screen name="Home" component={Home} />
+        </Stack.Navigator>
+    );
+};
+
+export default Router;
