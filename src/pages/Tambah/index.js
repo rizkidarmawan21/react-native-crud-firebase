80e483e7f7618982518de8b8cc56ff9ef84e330a
Rizki Darmawan <rizkidarmawan.0402102@gmail.com>
2021-12-25 09:43:13 +0700

    make func create

--- src/pages/Tambah/index.js
+++ src/pages/Tambah/index.js
@@ -1,53 +1,55 @@
 import React, { Component } from 'react'
-import { StyleSheet, View, ScrollView,TouchableOpacity,Text, Alert } from 'react-native'
+import { StyleSheet, View, ScrollView, TouchableOpacity, Text, Alert } from 'react-native'
 import { TextInput } from '../../components'
-// import FIREBASE from '../../config/Firebase'
 import { getDatabase, ref, push, set } from "firebase/database";
+import { FIREBASE } from '../../config/Firebase';
 
 
 export default class Tambah extends Component {
 
     constructor(props) {
         super(props)
-    
+
         this.state = {
-             kd_brg : "",
-             nm_brg : "",
-             satuan : "",
-             hrg_beli : "",
-             hrg_jual : "",
-             stok : "",
-             stok_min : ""
+            kd_brg: "",
+            nm_brg: "",
+            satuan: "",
+            hrg_beli: "",
+            hrg_jual: "",
+            stok: "",
+            stok_min: ""
         }
     }
 
-    OnChangeText = (namaState,value) => {
+    OnChangeText = (namaState, value) => {
         this.setState({
-            [namaState] : value
+            [namaState]: value
         })
     }
-    
+
     OnSubmit = () => {
-        if(this.state.kd_brg && this.state.nm_brg && this.state.satuan && this.state.hrg_beli && this.state.hrg_jual && this.state.stok && this.state.stok_min ) {
+        if (this.state.kd_brg && this.state.nm_brg && this.state.satuan && this.state.hrg_beli && this.state.hrg_jual && this.state.stok && this.state.stok_min) {
             console.log("data masuk on submit")
             console.log(this.state)
-            const db = getDatabase();
-            const postListRef = ref(db, 'Data');
+            const db = getDatabase(FIREBASE)
+            const postListRef = ref(db, 'posts');
             const newPostRef = push(postListRef);
             set(newPostRef, {
                 kd_brg: this.state.kd_brg,
-                nm_brg: this.state.nm_brg,
-                satuan: this.state.satuan,
-                hrg_beli: this.state.hrg_beli,
-                hrg_jual: this.state.hrg_jual,
-                stok: this.state.stok,
-                stok_min: this.state.stok_min
-            }).then((db) => {
+                    nm_brg: this.state.nm_brg,
+                    satuan: this.state.satuan,
+                    hrg_beli: this.state.hrg_beli,
+                    hrg_jual: this.state.hrg_jual,
+                    stok: this.state.stok,
+                    stok_min: this.state.stok_min
+            }).then((data) => {
                 Alert.alert("Success","Success Tambah Data");
                 this.props.navigation.replace('Home');
-            }).catch((error) => {
+            })
+            .catch((error) => {
                 console.log("Error :" ,error)
-            });
+            })
+;
 
             // const barang = {
             //     kd_brg: this.state.kd_brg,
@@ -69,76 +71,77 @@ export default class Tambah extends Component {
             //         console.log("Error :" ,error)
             //     })
 
-        }else {
-            Alert.alert("Error","Data wajib diisi !")
+        } else {
+            Alert.alert("Error", "Data wajib diisi !")
         }
     }
 
+    
     render() {
         return (
             <ScrollView>
                 <View style={styles.pages}>
-                    <TextInput 
-                    label="Kode Barang" 
-                    placeholder="Masukkan Kode Barang"
-                    OnChangeText = {this.OnChangeText}
-                    value = {this.state.kd_brg} 
-                    namaState = "kd_brg"
+                    <TextInput
+                        label="Kode Barang"
+                        placeholder="Masukkan Kode Barang"
+                        OnChangeText={this.OnChangeText}
+                        value={this.state.kd_brg}
+                        namaState="kd_brg"
                     />
 
-                    <TextInput 
-                    label="Nama Barang" 
-                    placeholder="Masukkan Nama Barang"
-                    OnChangeText = {this.OnChangeText}
-                    value = {this.state.nm_brg} 
-                    namaState = "nm_brg" 
+                    <TextInput
+                        label="Nama Barang"
+                        placeholder="Masukkan Nama Barang"
+                        OnChangeText={this.OnChangeText}
+                        value={this.state.nm_brg}
+                        namaState="nm_brg"
                     />
 
-                    <TextInput 
-                    label="Satuan" 
-                    placeholder="Masukkan Satuan Barang" 
-                    OnChangeText = {this.OnChangeText}
-                    value = {this.state.satuan} 
-                    namaState = "satuan"
+                    <TextInput
+                        label="Satuan"
+                        placeholder="Masukkan Satuan Barang"
+                        OnChangeText={this.OnChangeText}
+                        value={this.state.satuan}
+                        namaState="satuan"
                     />
 
-                    <TextInput 
-                    label="Harga Beli" 
-                    placeholder="Masukkan Harga Beli" 
-                    type='numeric'
-                    OnChangeText = {this.OnChangeText}
-                    value = {this.state.hrg_beli} 
-                    namaState = "hrg_beli"
+                    <TextInput
+                        label="Harga Beli"
+                        placeholder="Masukkan Harga Beli"
+                        type='numeric'
+                        OnChangeText={this.OnChangeText}
+                        value={this.state.hrg_beli}
+                        namaState="hrg_beli"
                     />
 
-                    <TextInput 
-                    label="Harga Jual" 
-                    placeholder="Masukkan Harga Jual" 
-                    type='numeric'
-                    OnChangeText = {this.OnChangeText}
-                    value = {this.state.hrg_jual} 
-                    namaState = "hrg_jual"
+                    <TextInput
+                        label="Harga Jual"
+                        placeholder="Masukkan Harga Jual"
+                        type='numeric'
+                        OnChangeText={this.OnChangeText}
+                        value={this.state.hrg_jual}
+                        namaState="hrg_jual"
                     />
 
-                    <TextInput 
-                    label="Stok Minimal" 
-                    placeholder="Masukkan Stok Minimal" 
-                    type='numeric'
-                    OnChangeText = {this.OnChangeText}
-                    value = {this.state.stok} 
-                    namaState = "stok"
+                    <TextInput
+                        label="Stok Minimal"
+                        placeholder="Masukkan Stok Minimal"
+                        type='numeric'
+                        OnChangeText={this.OnChangeText}
+                        value={this.state.stok}
+                        namaState="stok"
                     />
 
-                    <TextInput 
-                    label="Stok" 
-                    placeholder="Masukkan Stok" 
-                    type='numeric'
-                    OnChangeText = {this.OnChangeText}
-                    value = {this.state.stok_min} 
-                    namaState = "stok_min"
+                    <TextInput
+                        label="Stok"
+                        placeholder="Masukkan Stok"
+                        type='numeric'
+                        OnChangeText={this.OnChangeText}
+                        value={this.state.stok_min}
+                        namaState="stok_min"
                     />
 
-                    <TouchableOpacity style={styles.btnSubmit} onPress={() => this.OnSubmit() }>
+                    <TouchableOpacity style={styles.btnSubmit} onPress={() => this.OnSubmit()}>
                         <Text style={styles.textBtn}>Submit</Text>
                     </TouchableOpacity>
                 </View>
@@ -153,16 +156,16 @@ const styles = StyleSheet.create({
         padding: 30
     },
     btnSubmit: {
-        backgroundColor:"black",
-        padding:10,
-        borderRadius:5,
-        marginTop:10
+        backgroundColor: "#ff8f17",
+        padding: 10,
+        borderRadius: 5,
+        marginTop: 10
     },
     textBtn: {
-        color:'white',
-        fontWeight:'bold',
-        textAlign:'center',
-        fontSize:16
+        color: 'white',
+        fontWeight: 'bold',
+        textAlign: 'center',
+        fontSize: 16
     }
 
 
