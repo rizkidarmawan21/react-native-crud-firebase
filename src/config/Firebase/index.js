1904ce1acf2c85c7c959888ffe9bdf24d2cfec41
Rizki Darmawan <80609220+rizkidarmawan21@users.noreply.github.com>
2021-12-30 05:18:32 +0700

    deleted firebase key

--- src/config/Firebase/index.js
+++ src/config/Firebase/index.js
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBjQGCMUSNW14ykpWS0tg3m6vBGAC1Bh5g",
-  authDomain: "crud-react-native-c3912.firebaseapp.com",
-  projectId: "crud-react-native-c3912",
-  storageBucket: "crud-react-native-c3912.appspot.com",
-  messagingSenderId: "26398917425",
-  appId: "1:26398917425:web:5191ddc21653f199472610"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: ""
 };
 
 // Initialize Firebase
